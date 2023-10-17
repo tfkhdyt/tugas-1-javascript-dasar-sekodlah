838f41ada87e405f75f55a7d9acf82df59c63bbf
tfkhdyt <me@tfkhdyt.my.id>
2023-10-17 14:34:45 +0700

    feat: add soal 4

---
+++ Taufik_Hidayat-soal4-kelompok5.mjs
@@ -0,0 +1,7 @@
+function hitungLuasSegitiga(alas, tinggi) {
+  return (alas * tinggi) / 2;
+}
+
+const luasSegitiga = hitungLuasSegitiga(6, 9);
+
+console.log(`Luas segitiga dengan alas 6 dan tinggi 9 adalah: ${luasSegitiga}`);
